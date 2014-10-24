4093c1f085e37cf550d67a3ef8bfc19028d4a074
Adam Knight <adam.knight@opensquare.co.uk>
2014-10-24 15:40:10 +0100

    Added ability to store result in header or property to default processor

--- component/src/main/java/uk/co/revsys/esb/component/DefaultProcessor.java
+++ component/src/main/java/uk/co/revsys/esb/component/DefaultProcessor.java
@@ -7,9 +7,27 @@ import org.apache.camel.Processor;
 public abstract class DefaultProcessor implements Processor, ParameterMapAwareProcessor{
 
     private Map<String, Object> parameters;
+    private String header;
+    private String property;
     
     public void setParameters(Map<String, Object> parameters) {
         this.parameters = parameters;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
     }
 
     protected <P> P getParameter(Exchange exchange, String parameterName, Class<? extends P> type){
@@ -25,6 +43,16 @@ public abstract class DefaultProcessor implements Processor, ParameterMapAwarePr
             }
         }
         return parameter;
+    }
+    
+    protected void storeResult(Exchange exchange, Object result){
+        if(getHeader()!=null){
+            exchange.getIn().setHeader(getHeader(), result);
+        }else if(getProperty()!=null){
+            exchange.setProperty(getProperty(), result);
+        }else{
+            exchange.getIn().setBody(result);
+        }
     }
     
 }
